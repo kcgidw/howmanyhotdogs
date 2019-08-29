099985bb60f1d123577fa8babe3ec4b569afcb0e
kcgidw <kc.gideonw@gmail.com>
2019-08-28 21:07:08 -0700

    update result UI

--- source/index.jsx
+++ source/index.jsx
@@ -3,13 +3,14 @@ class MainComponent extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			input: 0,
+			input: '',
 			result: '',
+			showInvalid: false,
 		};
 
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
-		this.getDogsText = this.getDogsText.bind(this);
+		this.hasResult = this.hasResult.bind(this);
 	}
 	render() {
 		return (
@@ -23,26 +24,48 @@ class MainComponent extends React.Component {
 						<div>
 							$ <input type="text" onChange={this.handleChange} placeholder="0.00"></input> USD
 						</div>
+						{/* <div id="validation-comment" className={this.state.showInvalid ? '' : 'hidden'}>Invalid input</div> */}
 					</form>
 				</section>
 				<section id="result">
-					{this.getDogsText()}
+					<h1 id="result-value" className={this.hasResult() ? '' : 'hidden'}>
+						{this.state.result}
+					</h1>
+					<div id="result-desc" className={this.hasResult() ? '' : 'hidden'}>
+						Costco hot dogs*
+					</div>
 				</section>
 			</div>
 		);
 	}
+	hasResult() {
+		return this.state.result !== undefined && this.state.result !== '';
+	}
 	handleChange(evt) {
 		let elem = evt.target;
-		let newInput = +evt.target.value;
+		let inputVal = evt.target.value.trim();
+		let newInput = +inputVal;
 		// let regex = /^[0-9]*(\.[0-9]?[0-9]?)?$/;
-		if (isNaN(newInput)) {
+		if (isNaN(newInput)) { // invalid
 			elem.classList.add('invalid');
+			this.setState({
+				result: '',
+				showInvalid: true,
+			});
 		} else {
 			elem.classList.remove('invalid');
-			this.setState({
-				input: newInput,
-				result: toCHD(newInput),
-			});
+			if (inputVal === '') { // empty input
+				this.setState({
+					result: '',
+					showInvalid: false,
+				});
+			} else { // valid input
+				this.setState({
+					input: newInput,
+					result: roundCHD(toCHD(newInput)),
+					showInvalid: false,
+				});
+			}
 		}
 	}
 	handleSubmit(evt) {
@@ -50,19 +73,15 @@ class MainComponent extends React.Component {
 		inputElem.blur(); // close mobile keyboards
 		evt.preventDefault();
 	}
-	getDogsText() {
-		if(this.state.result === '') {
-			return '';
-		}
-		let strResult = Math.round(this.state.result * 10) / 10;
-		return (<p>{strResult} <span className="nowrap">Costco hot dogs</span></p>);
-	}
 }
 
 const dogPrice = 1.5;
 
 function toCHD(usdAmount) {
 	return usdAmount / dogPrice;
+}
+function roundCHD(amt) {
+	return Math.round(amt * 10) / 10;
 }
 
 ReactDOM.render(
